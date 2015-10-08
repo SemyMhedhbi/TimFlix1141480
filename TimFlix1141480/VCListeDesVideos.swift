2c43c20cad439bd3765183fad85d2f0bbd593094
etudiant <etudiant@iMac-de-Technicien.local>
2015-10-08 15:09:50 -0400

    les photos s'affichent

--- TimFlix1141480/VCListeDesVideos.swift
+++ TimFlix1141480/VCListeDesVideos.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class VCListeDesVideos: UIViewController {
 
+    var listeDesVideos:[Dictionary<String,String>] = []
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.à
-        /*let timFlixData:[Dictionary<String,String>] = [
-        
-        ] // listeDesVideos*/
+        // Créer un tableau à partir d'un fichier de type 'plist'
+        let pathFichierPlist = NSBundle.mainBundle().pathForResource("Listes des videos", ofType: "plist")!
+        listeDesVideos = NSArray(contentsOfFile: pathFichierPlist) as! Array
+        //println(listeDesVideos)
+    
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listeDesVideos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var unVideo = UITableViewCell()
+        
+        unVideo.textLabel!.text = listeDesVideos[indexPath.row]["nom"]
+        let fichierImage = listeDesVideos[indexPath.row]["pochette"]!
+        unVideo.imageView!.image = UIImage(named:fichierImage)
+        
+        return unVideo
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
